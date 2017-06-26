380dfd1cf9f48b381d94383a2acca24be9e8bc64
adam-p-king <devadamking@gmail.com>
2017-06-26 16:40:15 +0100

    Initial commit with existing sources

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_ioshmultichoice';
 $plugin->version   = 2012061700;
 
-$plugin->requires  = 2012061700;
+$plugin->requires  = 2015111610;
 
 $plugin->maturity  = MATURITY_STABLE;
